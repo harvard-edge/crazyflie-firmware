5c9f54eaad164420c9966fe30dd70a8a312ea394
Bart <b.p.duisterhof@student.tudelft.nl>
2019-07-15 17:15:10 -0400

    sensor bug

--- src/deck/drivers/src/sensor.c
+++ src/deck/drivers/src/sensor.c
@@ -27,14 +27,16 @@ void get_status(void){
 void init_interrupt(void){
 
     uint16_t lowerThreshold = 0;
-    uint16_t upperThreshold = 1;
+    uint16_t upperThreshold = 0;
     tsl2591Persist_t persist = TSL2591_PERSIST_ANY;
-
-    i2cdevWriteByte(I2C1_DEV, TSL2591_ADDR, TSL2591_COMMAND_BIT | TSL2591_REGISTER_PERSIST_FILTER,1);
-    i2cdevWriteByte(I2C1_DEV, TSL2591_ADDR, TSL2591_COMMAND_BIT | TSL2591_REGISTER_THRESHOLD_AILTL,lowerThreshold);
-    i2cdevWriteByte(I2C1_DEV, TSL2591_ADDR, TSL2591_COMMAND_BIT | TSL2591_REGISTER_THRESHOLD_AILTH,lowerThreshold >> 8);
-    i2cdevWriteByte(I2C1_DEV, TSL2591_ADDR, TSL2591_COMMAND_BIT | TSL2591_REGISTER_THRESHOLD_AIHTL,upperThreshold);
-    i2cdevWriteByte(I2C1_DEV, TSL2591_ADDR, TSL2591_COMMAND_BIT | TSL2591_REGISTER_THRESHOLD_AIHTH,upperThreshold >> 8);
+    enable();
+    i2cdevWriteByte(I2C1_DEV, TSL2591_ADDR, TSL2591_CLEAR_INT,0);
+//    i2cdevWriteByte(I2C1_DEV, TSL2591_ADDR, TSL2591_COMMAND_BIT | TSL2591_REGISTER_PERSIST_FILTER,1);
+//    i2cdevWriteByte(I2C1_DEV, TSL2591_ADDR, TSL2591_COMMAND_BIT | TSL2591_REGISTER_THRESHOLD_AILTL,lowerThreshold);
+//    i2cdevWriteByte(I2C1_DEV, TSL2591_ADDR, TSL2591_COMMAND_BIT | TSL2591_REGISTER_THRESHOLD_AILTH,lowerThreshold >> 8);
+//    i2cdevWriteByte(I2C1_DEV, TSL2591_ADDR, TSL2591_COMMAND_BIT | TSL2591_REGISTER_THRESHOLD_AIHTL,upperThreshold);
+//    i2cdevWriteByte(I2C1_DEV, TSL2591_ADDR, TSL2591_COMMAND_BIT | TSL2591_REGISTER_THRESHOLD_AIHTH,upperThreshold >> 8);
+    disable();
 
 }
 
@@ -59,6 +61,7 @@ void TSL2591_init(void){
     DEBUG_PRINT("Get ready...\n");
     systemWaitStart();
 
+    int found_sens = 0;
     //Using deckport
     if(i2cdevInit(I2C1_DEV))
     {
@@ -70,26 +73,33 @@ void TSL2591_init(void){
 
     // getting ID
     uint8_t id;
-    i2cdevReadByte(I2Cx,TSL2591_ADDR,TSL2591_COMMAND_BIT|TSL2591_REGISTER_DEVICE_ID,&id);
+    while(found_sens==0){
+        i2cdevReadByte(I2Cx,TSL2591_ADDR,TSL2591_COMMAND_BIT|TSL2591_REGISTER_DEVICE_ID,&id);
+        // check ID
+        if( id == 80)
+        {
+            DEBUG_PRINT("TSL2591 FOUND!\n");
+            found_sens =1;
+        }
+        else{
+            DEBUG_PRINT("Looking for sensor... \n");
+        }
+        vTaskDelay(M2T(200));
 
-    // check ID
-    if(id==80)
-    {
-        DEBUG_PRINT("TSL2591 FOUND!\n");
-    }
-    else{
-        DEBUG_PRINT("Something has gone wrong...check sensor wiring \n");
-        return;
     }
 
-    get_status(); //show interrupt status
-    enable();     //enable sensor
+
+    //get_status(); //show interrupt status
 
     // writing gain and integration to device
+    enable();
     if(i2cdevWriteByte(I2Cx, TSL2591_ADDR, TSL2591_COMMAND_BIT | TSL2591_REGISTER_CONTROL,
                        TSL2591_INTEGRATIONTIME_300MS | TSL2591_GAIN_MED)){
+        disable();
         DEBUG_PRINT("Gain and integration time set. \n");
     }
+    disable();
+    init_interrupt();
 
 }
 // returns light intensity
@@ -98,17 +108,17 @@ void TSL2591_init(void){
 uint16_t read_TSL2591(uint8_t mode)
 {
 
-    get_status();   //get interrupt status
+    //get_status();   //get interrupt status
 
     // vars to read data to
     uint8_t test_flag;
     uint16_t x;
     uint16_t y;
-
     //read single sensor value
+
     if(i2cdevRead16(I2Cx,TSL2591_ADDR,I2CDEV_NO_MEM_ADDR,1,&test_flag)) //test if still online
     {
-
+        enable();
         // allow ADC to do its work
         for (uint8_t d=0; d<=TSL2591_INTEGRATIONTIME_300MS; d++)
         {
@@ -118,8 +128,8 @@ uint16_t read_TSL2591(uint8_t mode)
 
         i2cdevRead(I2Cx,TSL2591_ADDR,TSL2591_REGISTER_CHAN0_LOW,2,&y); //chan0 - Full + IR
         i2cdevRead(I2Cx,TSL2591_ADDR,TSL2591_REGISTER_CHAN1_LOW,2,&x); //chan1 - IR only
+        disable();
         DEBUG_PRINT("Full %u, Infra: %u\n",y,x);
-
     }
     else {
         DEBUG_PRINT("Device lost.. \n");
